8ac02f4256e3080417b257ebfc221e50a1128f56
Alex Guevara <a.guevara23@gmail.com>
2018-06-09 15:01:12 -0400

    add resume, reorder navbar items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import classes from "./Navbar.css";
 import { Link } from "react-router-dom";
+import resume from "../../assets/AlexGuevara_Resume_2018.pdf";
+
 const Navbar = () => (
   <div className={classes.Navbar}>
     <ul className={classes.Menu}>
       <Link to="/">Home</Link>
-      <Link to="/about/">About</Link>
       <Link to="/portfolio">Portfolio</Link>
+      <Link to="/about/">About</Link>
+      <a href={resume} target="_blank">
+        Resume
+      </a>
     </ul>
   </div>
 );
