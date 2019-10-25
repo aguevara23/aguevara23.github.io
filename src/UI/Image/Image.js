5e510edae6f34912602bd2cd4eac61c884c7bfc7
Alexandre Guevara <alexguevara@Alexandres-MacBook-Pro.local>
2019-10-24 23:32:07 -0400

    ag/remove old porfolio projects

--- src/UI/Image/Image.js
+++ src/UI/Image/Image.js
@@ -1,26 +1,23 @@
-import React, { Fragment } from "react";
-import classes from "./Image.css";
+import React, { Fragment } from 'react';
+import classes from './Image.css';
 
-import Wischen from "../../assets/projectImages/wischen.jpg";
-import RealEstate from "../../assets/projectImages/realestate.jpg";
-import InteriorDesign from "../../assets/projectImages/interiordesign.jpg";
-import Splatter from "../../assets/projectImages/splatter.jpg";
-import MilennialGoT from "../../assets/projectImages/MilennialGoT.jpg";
-import Dribbble01 from "../../assets/projectImages/dribbble01.jpg";
-import ColorGame from "../../assets/projectImages/colorgame.jpg";
-import TivixWeather from "../../assets/projectImages/tivixweather.jpg";
+import Wischen from '../../assets/projectImages/wischen.jpg';
+import RealEstate from '../../assets/projectImages/realestate.jpg';
+import InteriorDesign from '../../assets/projectImages/interiordesign.jpg';
+import Splatter from '../../assets/projectImages/splatter.jpg';
+import Dribbble01 from '../../assets/projectImages/dribbble01.jpg';
 
 // uses different styles depending on type prop
 const image = props => {
   let style = null;
-  props.type === "main"
+  props.type === 'main'
     ? (style = classes.MainImage)
     : (style = classes.BGImage);
 
   // loads different Images depending on the source prop
   const loadImage = () => {
     switch (props.source) {
-      case "Wischen":
+      case 'Wischen':
         return (
           <img
             className={style}
@@ -28,7 +25,7 @@ const image = props => {
             alt="A react app that lets people create a watchlist of movies"
           />
         );
-      case "Real Estate Website":
+      case 'Real Estate Website':
         return (
           <img
             className={style}
@@ -36,7 +33,7 @@ const image = props => {
             alt="A responsive redesign for a real estate website"
           />
         );
-      case "Splatter":
+      case 'Splatter':
         return (
           <img
             className={style}
@@ -44,15 +41,7 @@ const image = props => {
             alt="An fun app for creating simple splatter art"
           />
         );
-      case "Resume Booster":
-        return (
-          <img
-            className={style}
-            src={MilennialGoT}
-            alt="A fun app based on random data generators"
-          />
-        );
-      case "Interior Design":
+      case 'Interior Design':
         return (
           <img
             className={style}
@@ -60,28 +49,12 @@ const image = props => {
             alt="A portfolio website for an interior designer"
           />
         );
-      case "Landing Page":
+      case 'Landing Page':
         return (
           <img
             className={style}
             src={Dribbble01}
             alt="A mobile responsive landing page"
-          />
-        );
-      case "Guess the Color":
-        return (
-          <img
-            className={style}
-            src={ColorGame}
-            alt="A react native game based on color"
-          />
-        );
-      case "Weather Forecast":
-        return (
-          <img
-            className={style}
-            src={TivixWeather}
-            alt="A 5-day temperature forecast"
           />
         );
       default:
